a094453d5cb7bad355c0b05b6472425bc7c9e72c
Huruido-13 <casterfox@outlook.com>
2022-02-23 18:49:37 +0900

    add comment in code

--- main.go
+++ main.go
@@ -10,17 +10,17 @@ import (
 
 /*var src = os.Args[1]
 var dest = os.Args[2]*/
-const dst = "C:/Users/Oniichan/Desktop/pngandsketch"
-const src = "C:/Users/Oniichan/Desktop/画像まとめ/imgur 2022~"
-const maked_dir = dst + "/dstDirectory"
-
+const src = "C:/Users/Oniichan/Desktop/pngandsketch"
+const dst = "C:/Users/Oniichan/Desktop/画像まとめ/imgur 2022~"
+//初めのループだとファイルパスが "/"で区切られてるためループカウントでキャッチする
 var loop_count = 0
 
 func main() {
-	err := os.MkdirAll(maked_dir, 0777)
-	if err != nil {
+	//dstパスのディレクトリをサブディレクトリまですべて作成
+	err := os.MkdirAll(dst,0777)
+	if err != nil{
 		fmt.Println(err)
-		fmt.Println("ディレクトリはすでに存在します")
+		return
 	}
 	filepath.Walk(src, list_files)
 }
@@ -30,26 +30,18 @@ func list_files(path string, info fs.FileInfo, err error) error {
 		return err
 	}
 
-	files, err := filepath.Glob(path + "/*")
-
-	fmt.Println(files)
-
-	if err != nil {
-		return err
-	}
-
-	rename_result := rename_files(path, maked_dir)
+	rename_result := rename_files(path)
 
 	loop_count++
 
-	if rename_result {
+	if rename_result == nil{
 		return nil
 	}
 
 	return err
 }
 
-func rename_files(path, dst string) bool {
+func rename_files(path string) error {
 	var file_splited []string
 	if loop_count == 0 {
 		file_splited = strings.Split(path, "/")
@@ -57,18 +49,25 @@ func rename_files(path, dst string) bool {
 		// fmt.Println(dst + "/" + file_splited[file_index]) テスト用
 		dir_or_file, _ := os.Stat(path)
 		if !dir_or_file.IsDir() {
-			os.Rename(src, dst+"/"+file_splited[file_index])
+			dst_filepath := "/"+file_splited[file_index]
+			os.Rename(src + dst_filepath,  dst + dst_filepath)
 		}
-		return true
+		return nil
 	}
 
+	//ファイルpathを分割し[]stringで返す
 	file_splited = strings.Split(path, "\\")
+	//配列の最後のインデックス番号を取得。配列の最後に入っているファイルを取り出すための準備
 	file_index := len(file_splited) - 1
 	// fmt.Println(dst + "/" + file_splited[file_index]) テスト用
+	//ファイルもしくはディレクトリが存在していればfileinfoを返す。なければerrorを返す
 	dir_or_file, _ := os.Stat(path)
+	//pathから読み取ったものがディレクトリかどうかを判断
+	//ディレクトリなら次のファイルへ。ファイルならRenameで移動させる。
 	if !dir_or_file.IsDir() {
-		os.Rename(src, dst+"/"+file_splited[file_index])
+		dst_filepath := "/"+file_splited[file_index]
+		os.Rename(src + dst_filepath, dst + dst_filepath)
 	}
 
-	return true
+	return nil
 }
